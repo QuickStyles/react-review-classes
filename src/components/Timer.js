391367f417328d80aad6254e81aa2f8b8da35b43
Brandon Y Lam <me@brandonlam.ca>
2020-06-22 11:43:32 -0700

    add and remove timers

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,62 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// Class Components VS Functional Components:
-
-// Class
-// - access to state
-// - access to lifecycle methods
-
-// Functional
-// - Simpler
-
-/**
- * dsfjadslfjdslfjsdldfsjlfkdjs
- * 
- * @param {number} startTime - This is the start time of the component
- * @param {number} tickValue - This is the amount the timer increases per second
- */
-class Timer extends Component {
-  // constructor method gets called everytime an instance of the class is created
-  constructor(props) {
-    // in react the constructor always takes in a props object.
-    // we must always call super(props);
-    super(props);
-
-    // State allows an individual instance of a component to hold their own data.
-    this.state = {
-      seconds: this.props.startTime
-    }
-  }
-
-  tick() {
-    // if you need to update the state you can only use this.setState
-    this.setState((state) => {
-      // state is the current state of the component
-      // setState should always return an object. This object gets merged into the current state.
-      return {
-        seconds: state.seconds + this.props.tickValue
-      }
-    })
-  }
-
-  // this method gets triggered when the component (node) mounts onto the DOM
-  componentDidMount() {
-    setInterval(() => {
-      this.tick();
-    }, 1000)
-  }
-
-  // Class Components use the render() method to return JSX
-  // we have access to the state by calling this.state
-  render() {
-    console.log(this.props);
-    console.log('rendering...' + ' ' + this.state.seconds)
-    return(
-      <div>
-        { this.state.seconds }
-      </div>
-    )
-  }
+function Timer(props) {
+  return(
+    <div>
+      {props.currentTick}
+    </div>
+  )
 }
 
 export default Timer
--- src/components/TimerContainer.js
+++ src/components/TimerContainer.js
@@ -1,13 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Timer from './Timer';
 
-function TimerContainer() {
-  return(
-    <div>
-      <Timer startTime={10} tickValue={2}/>
-      <Timer startTime={5} tickValue={20}/>
-    </div>
-  )
+class TimerContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [
+        {
+          currentTick: 10,
+          tickValue: 2
+        },
+        {
+          currentTick: 5,
+          tickValue: 20
+        }
+      ]
+    }
+  }
+
+  tick() {
+    this.setState((state) => {
+      // const listCopy = [...state.list]
+      const listCopy = JSON.parse(JSON.stringify(state.list)) // copies state.list
+      const newList = listCopy.map((node) => { // creates a new array where every node's currentTick is increased by tickValue
+        return {
+          currentTick: node.currentTick + node.tickValue,
+          tickValue: node.tickValue
+        }
+      })
+      // merge this object into the state
+      return {
+        list: newList
+      }
+    })
+  }
+
+  addTimer() {
+    this.setState((state) => {
+      const listCopy = JSON.parse(JSON.stringify(state.list))
+      return {
+        list: [
+          ...listCopy, // copy of all the existing timers
+          {
+            currentTick: 0,
+            tickValue: 1
+          }
+        ]
+      }
+    })
+  }
+
+  removeTimer() {
+    this.setState((state) => {
+      const listCopy = JSON.parse(JSON.stringify(state.list))
+      listCopy.pop();
+      return {
+        list: listCopy
+      }
+    })
+  }
+
+  componentDidMount() {
+    setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  render() {
+    return(
+      <div>
+        {
+          this.state.list.map((node, i) => {
+            return <Timer key={i} currentTick={node.currentTick} tickValue={node.tickValue} />
+          })
+        }
+        {/* onClick is a synthetic event. They're just like DOM events. */}
+        <button onClick={(e) => this.addTimer()}>Add Timer</button>
+        <button onClick={(e) => this.removeTimer()}>Remove Timer</button>
+      </div>
+    )
+  }
 }
 
 export default TimerContainer
